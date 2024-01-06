6c539e5cf64a699737f81bffcc2b0162e13f871e
zina1t <79532105+zina1t@users.noreply.github.com>
2024-01-06 20:43:29 +0000

    Added 5th challenge 	new file:   Sea Level Predictor/README.md 	new file:   Sea Level Predictor/__pycache__/sea_level_predictor.cpython-310.pyc 	new file:   Sea Level Predictor/__pycache__/test_module.cpython-310.pyc 	new file:   Sea Level Predictor/epa-sea-level.csv 	new file:   Sea Level Predictor/main.py 	new file:   Sea Level Predictor/sea_level_plot.png 	new file:   Sea Level Predictor/sea_level_predictor.py 	new file:   Sea Level Predictor/test_module.py

---
+++ Sea Level Predictor/sea_level_predictor.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def draw_plot():
+    # Read data from file
+    df = pd.read_csv('/workspaces/freeCodeCampProjects/Sea Level Predictor/epa-sea-level.csv')
+
+    # Create scatter plot
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'])
+
+    # Create first line of best fit
+    res = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x_pred = pd.Series([i for i in range(1880, 2051)])
+    y_pred = res.slope*x_pred + res.intercept
+    plt.plot(x_pred, y_pred, 'r')
+    
+    # Create second line of best fit
+    df_new = df.loc[df['Year'] >= 2000]
+    res_new = linregress(df_new['Year'], df_new['CSIRO Adjusted Sea Level'])
+    x_pred_new = pd.Series([i for i in range(2000, 2051)])
+    y_pred_new = res_new.slope*x_pred_new + res_new.intercept
+    plt.plot(x_pred_new, y_pred_new, 'g') 
+    
+    # Add labels and title
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    
+    # Save plot and return data for testing (DO NOT MODIFY)
+    plt.savefig('sea_level_plot.png')
+    return plt.gca()
